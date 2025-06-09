d4f711549354f6921dcee16d6ae0228ab862bfae
denec <dzenevich@mail.ru>
2025-06-09 22:24:44 +0300

    cartridge checksum assert and metadata

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,41 @@ fn print_half(bytes: &[u8]) {
     }
 }
 
+fn decode_ram_size(code: u8) -> String {
+    match code { 
+        0 => String::from("No RAM"),
+        1 => String::from("Unused"),
+        2 => String::from("1 bank"),
+        3 => String::from("4 banks of 8 KiB each"),
+        4 => String::from("16 banks of 8 KiB each"),
+        5 => String::from("8 banks of 8 KiB each"),
+        _ => panic!("invalid code") 
+    }
+}
+
+fn calculate_checksum(content: &[u8]) -> u8 {
+    let mut checksum: u8 = 0;
+    
+    for address in 0x0134..0x014C + 1 {
+        checksum = checksum.wrapping_sub(content[address] + 1);
+    }
+    
+    checksum
+}
+
+fn calculate_global_checksum(content: &[u8]) -> u16 {
+    let mut checksum: u16 = 0;
+    
+    for address in 0..content.len() {
+        if address == 0x014E || address == 0x014F {
+            continue;
+        }
+        checksum = checksum.wrapping_add(content[address] as u16);
+    }
+    
+    checksum
+}
+
 fn main() {
     let content = fs::read("hello-world.gb").unwrap();
     
@@ -46,4 +81,14 @@ fn main() {
     }
 
     print_nintendo_logo(nintendo_logo_content);
+    
+    println!("Cartridge type: {}", format!("{:0x}", content[0x147]));
+    println!("ROM size: 32 KiB * {}", 1 << content[0x148]);
+    println!("RAM size: {}", decode_ram_size(content[0x149]));
+    
+    let header_checksum = content[0x14D];
+    assert_eq!(header_checksum, calculate_checksum(&content));
+    
+    let global_checksum = ((content[0x14E] as u16) << 8) + (content[0x14F] as u16);
+    assert_eq!(global_checksum, calculate_global_checksum(&content));
 }
